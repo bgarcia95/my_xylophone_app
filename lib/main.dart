f23fe68bbac5ec796a2639e1ed1d104a37b03553
bgarcia95 <bgarcia.95@outlook.com>
2019-06-19 22:07:54 -0600

    App finished.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final player = AudioCache();
+
+  void playNote(int soundNumber) {
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey({Color colorPicked, int soundNumber}) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {
+          playNote(soundNumber);
+        },
+        color: colorPicked,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
-            child: Row(
-          children: <Widget>[],
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            buildKey(colorPicked: Colors.red, soundNumber: 1),
+            buildKey(colorPicked: Colors.orange, soundNumber: 2),
+            buildKey(colorPicked: Colors.yellow, soundNumber: 3),
+            buildKey(colorPicked: Colors.green, soundNumber: 4),
+            buildKey(colorPicked: Colors.teal, soundNumber: 5),
+            buildKey(colorPicked: Colors.blue, soundNumber: 6),
+            buildKey(colorPicked: Colors.purple, soundNumber: 7),
+          ],
         )),
       ),
     );
