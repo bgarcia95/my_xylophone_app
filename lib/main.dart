d59b4af97dd8f4dcdca4dc37c4d9eb70d4d1582e
bgarcia95 <bgarcia.95@outlook.com>
2019-06-19 21:53:26 -0600

    Starting project. Dependencies added.

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+            child: Row(
+          children: <Widget>[],
+        )),
+      ),
+    );
+  }
+}
